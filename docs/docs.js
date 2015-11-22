1e4b505b1f4c5464621e1232744f01520bc8a51b
Walunt Geek <wg@walnutgeek.com>
2015-11-21 22:30:07 -0800

    remove unused file

--- docs/docs.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello");
-module.exports = function(sources,title,hasTitle,sections){
-  console.log(JSON.stringify({sources:sources,title:title,hasTitle:hasTitle, sections:sections}));
-}
