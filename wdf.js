abe0953e5aab783e5653f696e72065ad6a84179b
Walunt Geek <wg@walnutgeek.com>
2016-01-22 07:33:22 -0800

    Updates

--- wdf.js
+++ wdf.js
@@ -46,8 +46,11 @@ var wdf =
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = {
-	  utils: __webpack_require__(1),
+	  utils:     __webpack_require__(1),
 	  DataFrame: __webpack_require__(5),
+	  WdfView:   __webpack_require__(6),
+	  WebPath:   __webpack_require__(8),
+	  ViewTheme: __webpack_require__(7),
 	};
 
 /***/ },
@@ -16151,6 +16154,619 @@ var wdf =
 	
 	
 	})();
+
+/***/ },
+/* 6 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function() {
+	  "use strict";
+	
+	  var _ = __webpack_require__(2);
+	  var u$ = __webpack_require__(1);
+	  var DataFrame = __webpack_require__(5);
+	
+	  var defaults = {
+	    format: null,
+	    widths: {},
+	    theme: __webpack_require__(7),
+	    max_width: 300,
+	  } ;
+	
+	  var getUniqueId =
+	      ( function () {
+	        var incrementingId = 0;
+	        return function() {
+	          return incrementingId++;
+	        };
+	      })();
+	
+	  function setAllAttributes(elem, attrs){
+	    if(attrs){
+	      for(var k in attrs){
+	        if(attrs.hasOwnProperty(k)){
+	          elem.setAttribute(k,attrs[k]);
+	        }
+	      }
+	    }
+	  }
+	
+	  function render_value(div,v){
+	    if( u$.isPrimitive(v) ){
+	      div.innerText = v ;
+	    }else{
+	      div.appendChild(v);
+	    }
+	  }
+	  function WdfView(props){
+	    props = _.defaults(props,defaults);
+	    this.props = props;
+	    if(!this.props.df){
+	      throw u$.error({msg: 'df parameter has to be defined'});
+	    }
+	    this.df = this.props.df ;
+	    if(!this.props.document){
+	      throw u$.error({msg: 'document parameter has to be defined'});
+	    }
+	    if( this.props.container ){
+	      if( _.isString(this.props.container) ){
+	        var queried = this.props.document.querySelector(this.props.container);
+	        if(!queried){
+	          throw u$.error({
+	            msg:'container query does not match anything.',
+	            query: this.props.container});
+	        }
+	        this.props.container = queried;
+	      }
+	      this.props.container.innerHTML='';
+	      this.props.container.wdfView = this ;
+	    }
+	    this.id = 'wdf_id_' + getUniqueId();
+	    this.header =  this.new_elem(this.props.container, 'table', ['wdf','wdf_header', this.id]);
+	    this.data =    this.new_elem(this.props.container, 'table', ['wdf','wdf_data',this.id]);
+	    var head_tr =  this.new_elem(this.header, 'tr',['wdf']);
+	
+	    function get_formatter(name,arg){
+	      return props.theme[name](props.format,arg);
+	    }
+	
+	    var header_cell_fn = get_formatter('header_cell_fn');
+	    var columnNames = this.df.getColumnNames();
+	    var cell_fns = [] ;
+	    var r, tr, th, td, div, col_name;
+	    for(var col_idx = 0 ; col_idx < columnNames.length; col_idx++ ){
+	      col_name = columnNames[col_idx];
+	      cell_fns[col_idx]=get_formatter('cell_fn',this.df.columnSet.byIndex[col_idx]);
+	      th = this.new_elem(head_tr,'th',['wdf'],
+	          {'data-column':col_name});
+	      div = this.new_elem(th,'div',['wdf_masker']);
+	      r = header_cell_fn.call(th, this, col_idx, col_name);
+	      if( !_.isUndefined(r) ){
+	        if( _.isPlainObject(r) ){
+	          render_value(div,r.value);
+	          setAllAttributes(div,r.div_attrs);
+	          setAllAttributes(th,r.th_attrs);
+	        }else{
+	          render_value(div,r);
+	        }
+	      }
+	    }
+	
+	    r = get_formatter('header_row_fn').call(head_tr,this);
+	    if( _.isPlainObject(r) ) {
+	      setAllAttributes(head_tr, r);
+	    }
+	
+	    var row_fn = get_formatter('row_fn');
+	    for(var row_idx = 0 ; row_idx < this.df.getRowCount(); row_idx++ ){
+	      var odd_even = 'wdf_' + (row_idx % 2 ? 'odd' : 'even');
+	      tr = this.new_elem(this.data,'tr',[ 'wdf',  odd_even ],
+	          {'data-row':row_idx});
+	      for( col_idx = 0 ; col_idx < columnNames.length; col_idx++ ){
+	
+	        col_name = columnNames[col_idx];
+	        td = this.new_elem(tr,'td',['wdf'],
+	            {'data-column':col_name});
+	        div = this.new_elem(td,'div',['wdf_masker']);
+	        r = cell_fns[col_idx].call(td, this, row_idx, col_idx, col_name);
+	
+	        if( !_.isUndefined(r) ){
+	          if( _.isPlainObject(r) ){
+	            render_value(div,r.value);
+	            setAllAttributes(div,r.div_attrs);
+	            setAllAttributes(td,r.td_attrs);
+	          }else{
+	            render_value(div,r);
+	          }
+	        }
+	        r = row_fn.call(tr, this, row_idx);
+	        if( _.isPlainObject(r) ) {
+	          setAllAttributes(tr, r);
+	        }
+	      }
+	    }
+	    this.widths = this.getColumnWidthStats();
+	    this.setAllColumnWidths();
+	    this.markOverflownColumn();
+	  }
+	
+	  WdfView.setDefault=function(key,value){
+	    defaults[key]=value;
+	  };
+	
+	  WdfView.getDefault=function(key){
+	    return defaults[key];
+	  };
+	
+	  WdfView.hasDefault=function(key){
+	    return defaults.hasOwnProperty(key);
+	  };
+	
+	  u$.jail(function(){
+	    WdfView.setDefault('document', document);
+	  });
+	
+	  u$.jail(function(){
+	    WdfView.setDefault('jQuery', jQuery);
+	    jQuery.fn.WdfView=function(props){
+	      if (this && this[0]){
+	        props = _.defaults(props,{container: this[0]});
+	        new WdfView(props);
+	      }
+	    };
+	  });
+	
+	  WdfView.prototype.new_elem = function (parent, tag , classes, attrs){
+	    var e = this.props.document.createElement(tag);
+	    if(classes){
+	      classes.forEach(function(c){ e.classList.add(c); });
+	    }
+	    setAllAttributes(e,attrs);
+	    if(parent) parent.appendChild(e);
+	    return e;
+	  };
+	
+	  WdfView.prototype.applyToColumn = function(col,fn){
+	    var colName = this.df.getColumnName(col);
+	    var q = '.'+this.id+' [data-column='+ colName +']' ;
+	    var elems = this.props.document.querySelectorAll(q);
+	    for(var i = 0 ; i < elems.length; i++ ){
+	      var cell = elems[i] ;
+	      var row = cell.parentElement.getAttribute('wdf_row');
+	      fn.call(this,row,colName,cell);
+	    }
+	  };
+	
+	  WdfView.prototype.applyToAllCells = function(fn){
+	    var q = '.'+this.id+' tr' ;
+	    var elems = this.props.document.querySelectorAll(q);
+	    for(var i = 0 ; i < elems.length; i++ ){
+	      var tr = elems[i];
+	      var row = tr.getAttribute('wdf_row');
+	      for(var j = 0 ; j < tr.childNodes.length; j++){
+	        var cell = tr.childNodes[j];
+	        var col_name = cell.getAttribute('data-column');
+	        if(!_.isNull(col_name)){
+	          fn.call(this,row,col_name,cell);
+	        }
+	      }
+	    }
+	  };
+	  function markOverflownCell( row, col_name, cell) {
+	    var real_w = cell.firstChild.scrollWidth;
+	    var visual_w = cell.firstChild.offsetWidth;
+	    if (real_w > visual_w) {
+	      cell.classList.add('wdf_over');
+	    } else {
+	      cell.classList.remove('wdf_over');
+	    }
+	  }
+	
+	
+	  WdfView.prototype.setColumnWidth = function(col,width){
+	    var colName = this.df.getColumnName(col);
+	    this.widths[colName].current = width ;
+	    this.applyToColumn(col,function(row,col_name,cell){
+	      cell.firstChild.style.width = width + 'px';
+	    });
+	    this.applyToColumn(col,markOverflownCell);
+	  };
+	
+	  WdfView.prototype.setAllColumnWidths = function(){
+	    this.applyToAllCells(function(row,col_name,cell){
+	      var col_width = this.widths[col_name];
+	      if( !col_width.current  ){
+	        col_width.current = col_width.max > this.props.max_width ?
+	            this.props.max_width : col_width.max;
+	      }
+	      cell.firstChild.style.width = col_width.current + 'px';
+	    });
+	  };
+	
+	
+	  WdfView.prototype.markOverflownColumn = function(){
+	    this.applyToAllCells(markOverflownCell);
+	  };
+	
+	  WdfView.prototype.getColumnWidthStats = function(){
+	    var real_width_stats = {};
+	    this.applyToAllCells(function(row,col_name,cell){
+	      var real_w = cell.firstChild.scrollWidth ;
+	      u$.collect_stats(col_name,real_w,real_width_stats);
+	    });
+	    return real_width_stats;
+	  };
+	
+	  module.exports = WdfView;
+	})();
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+	// ## Private stuff
+	//
+	// methods and classes that not supposed to be used directly
+	(function() {
+	  "use strict";
+	
+	  // cell_formatter:
+	  //
+	  // when called `this` is `<td>` element
+	  //
+	  // signature `function(view, row_idx, col_idx, col_name)`
+	  // returns:
+	  // `{ value: <cell value>,
+	  //    div_attrs: { <name> : <value> ,...}
+	  //    td_attrs: { <name> : <value> ,...}
+	  // }`
+	  // , or just `<cell value>` if no attributes on tags
+	  // need to be set, or `undefined` to indicate that columns
+	  // not need to be set.
+	
+	  // header_cell_formatter:
+	  //
+	  // when called `this` is `<th>` element
+	  //
+	  // signature `function(view, col_idx, col_name)`
+	  // returns:
+	  // `{ value: <cell value>,
+	  //    div_attrs: { <name> : <value> ,...}
+	  //    th_attrs: { <name> : <value> ,...}
+	  // }`
+	
+	  // row_formatter:
+	  //
+	  // when called `this` is `<tr>` element
+	  //
+	  // signature `function(view, row_idx)`
+	  // returns tr attributes:
+	  // `{ <name> : <value> ,...}`
+	
+	  // header_row_formatter:
+	  //
+	  // when called `this` is `<tr>` element
+	  //
+	  // signature `function(view)`
+	  // returns tr attributes:
+	  // `{ <name> : <value> ,...}`
+	
+	  function get_cell_as_string(view,row_idx,col_idx,col_name) {
+	    return view.df.get(row_idx,col_idx, 'as_string');
+	  }
+	  exports.get_cell_as_string = get_cell_as_string;
+	
+	  function get_cell(view,row_idx,col_idx,col_name) {
+	    return view.df.get(row_idx,col_idx);
+	  }
+	  exports.get_cell = get_cell;
+	
+	  // formatters library
+	  function anchor_elem(view, link) {
+	    var a = view.new_elem(null, 'a', ['wdf_link'], {href: link.href});
+	    a.innerText = link.text || link.href;
+	    return a;
+	  }
+	  exports.anchor_elem = anchor_elem ;
+	
+	  var FORMAT = {
+	    cell_by_type: {
+	      date: get_cell_as_string,
+	      datetime: get_cell_as_string,
+	      timestamp: get_cell_as_string,
+	      link: function(view,row_idx,col_idx,col_name) {
+	        var link = view.df.get(row_idx,col_idx);
+	        if(link){
+	          return anchor_elem(view, link);
+	        }
+	        return undefined;
+	      }
+	    },
+	    other_cell: get_cell,
+	    header_cell: function(view,col_idx,col_name){
+	      return col_name;
+	    },
+	    row: function(view,row_idx){},
+	    header_row: function(view){}
+	  };
+	
+	  exports.FORMAT =FORMAT;
+	
+	  function find_format_fn(format,col){
+	    var fn;
+	    if(col && format){
+	      if(format.cell_by_name){
+	        fn =  format.cell_by_name[col.name] ;
+	      }
+	      if(!fn && format.cell_by_type && col.type){
+	        fn = format.cell_by_type[col.type.name];
+	      }
+	    }
+	    if( !fn && format ) {
+	      fn = format.other_cell;
+	    }
+	    return fn;
+	  }
+	
+	  exports.header_cell_fn=function(format){
+	    return ( format ? format.header_cell : null ) || FORMAT.header_cell;
+	  };
+	
+	  exports.cell_fn=function(format, col){
+	    return find_format_fn(format,col) || find_format_fn(FORMAT,col);
+	  };
+	
+	  exports.row_fn=function(format){
+	    return ( format ? format.row : null ) || FORMAT.row;
+	  };
+	
+	  exports.header_row_fn = function (format){
+	    return ( format ? format.header_row : null ) || FORMAT.header_row;
+	  };
+	
+	})();
+
+/***/ },
+/* 8 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function() {
+	    "use strict";
+	    var u$ = __webpack_require__(1);
+	    var _ = __webpack_require__(2);
+	
+	    var SEARCH_TYPES = {
+	        'R': function(pattern){
+	                return new RegExp(pattern);
+	        },//regex case sensitive
+	        'r': function(pattern){
+	            return new RegExp(pattern,'i');
+	        },//regex case insensitive
+	        'S': function(pattern){
+	            return {
+	                test: function(s){
+	                    return s.indexOf(pattern) > -1;
+	                }};
+	        },//contains
+	        's': function(pattern){
+	            var low_pattern = pattern.toLowerCase();
+	            return {
+	                test: function (s) {
+	                    return s.toLowerCase().indexOf(low_pattern) > -1;
+	                }};
+	        }};
+	
+	    function Search(s) {
+	        if( ! SEARCH_TYPES.hasOwnProperty(s[0])) {
+	            throw u$.error({ msg:'no such search type',
+	                type: s[0], allowed:Object.keys(SEARCH_TYPES) } );
+	        }
+	        this.type = s[0];
+	        this.pattern = s.substring(1);
+	        this.matcher = SEARCH_TYPES[this.type](this.pattern);
+	    }
+	
+	    Search.prototype.toString=function(){
+	        return this.type + this.pattern;
+	    };
+	    Search.prototype.test=function(s){
+	      return this.matcher.test(s);
+	    };
+	
+	    function check(c, allowed){
+	        if(allowed.indexOf(c) < 0){
+	            throw u$.error({msg:'not allowed',
+	                provided:c, allowed:allowed});
+	        }
+	        return c;
+	    }
+	    function Field(s){
+	        this.and_or = 'A';
+	        this.show_hide = 'S';
+	        this.accending_descending = null ;
+	        this.sort_index = null ;
+	        this.searches = null;
+	        if( _.isString(s) && s !== "" ){
+	            this.and_or = check(s[0],['A','O']);
+	            this.show_hide = check(s[1],['S','H']);
+	            var i = 2;
+	            if(['A','D'].indexOf(s[i]) > -1){
+	                this.accending_descending = s[i];
+	                while( ++i < s.length && s[i] >= '0' && s[i] <= '9');
+	                this.sort_index = +s.substring(3,i);
+	            }
+	            var search_array = [] ;
+	            var next_search = '' ;
+	            for( ; i < s.length ; i++){
+	                var c = s[i], nc = s[i+1];
+	                if(c === ',' ){
+	                    if( nc === ',' ){
+	                        i++;
+	                        next_search+=c;
+	                    }else{
+	                        search_array.push(new Search(next_search));
+	                        next_search = '';
+	                    }
+	                }else{
+	                    next_search+=c;
+	                }
+	            }
+	            if(next_search.length){
+	                search_array.push(new Search(next_search));
+	            }
+	            if(search_array.length){
+	                this.searches = search_array;
+	            }
+	        }
+	    }
+	    function escape_commas(s){
+	        if( s.indexOf(',') < 0){
+	            return s;
+	        }
+	        var copy = '', c;
+	        for(var i = 0 ; i < s.length; i++){
+	            c = s[i];
+	            copy +=  c ;
+	            if( c == ',' ){
+	                copy += c ;
+	            }
+	        }
+	        return copy;
+	    }
+	
+	    Field.prototype.test = function(s){
+	      if( !this.searches ){
+	        return true;
+	      }
+	      var i;
+	      if(  this.and_or === 'A' ){
+	        for(i = 0 ; i < this.searches.length; i++ ){
+	          if( !this.searches[i].test(s) ){
+	            return false;
+	          }
+	        }
+	        return true ;
+	      }else{
+	        for(i = 0 ; i < this.searches.length; i++ ){
+	          if( this.searches[i].test(s) ){
+	            return true;
+	          }
+	        }
+	        return false;
+	      }
+	    };
+	
+	    Field.prototype.toString = function(){
+	        var s = this.and_or + this.show_hide;
+	        if( this.accending_descending && _.isNumber(this.sort_index) ){
+	            s += this.accending_descending + this.sort_index;
+	        }
+	        if(this.searches && this.searches.length){
+	            for(var i = 0 ; i < this.searches.length; i++){
+	                if(i) s += ',';
+	                s += escape_commas(this.searches[i].toString());
+	            }
+	        }
+	        return s === 'AS' ? undefined : s;
+	    };
+	
+	    function Params(input){
+	      var self = this ;
+	      input = input || '';
+	      input.split('&').forEach(function(s){
+	        if(s){
+	          var key_val = s.split('=',2);
+	          self[key_val[0]] = new Field(decodeURIComponent(key_val[1]));
+	        }
+	      });
+	    }
+	
+	    Params.prototype.toString=function(){
+	      var s = '';
+	      for (var key in this) {
+	        if (!this.hasOwnProperty(key) ) continue;
+	        var v = this[key].toString() ;
+	        if( v ){
+	          if( s ){
+	            s += '&' ;
+	          }
+	          s += key + '=' +  encodeURIComponent(v) ;
+	        }
+	      }
+	      return s ;
+	    };
+	
+	    function WebPath(input){
+	        var array = input ;
+	        if(_.isString(input)){
+	            var posParams = input.indexOf('?');
+	            if(posParams >= 0){
+	                this.params =  new Params(input.substring(posParams+1));
+	                input = input.substring(0,posParams);
+	            }
+	            array = input.split('/');
+	            this.name = array.pop();
+	            if( this.name === "" ){
+	                this.name = array.pop();
+	                this.dir = true ;
+	            }
+	        }else{
+	            // if it is not string that it is recursive call
+	            // to init parent chain
+	            this.name = array.pop() ;
+	            this.dir = true;
+	        }
+	        this.parent = array.length > 0 ?  new WebPath(array) : null ;
+	    }
+	
+	    WebPath.prototype.extension=function(){
+	      if( _.isUndefined(this.ext) ){
+	        for( var i = this.name.length - 1 ; i > 0 ; i-- ){
+	          if(this.name[i] === '.' ){
+	            this.ext = this.name.substr(i+1).toLowerCase();
+	            return this.ext;
+	          }
+	        }
+	        this.ext = null ;
+	      }
+	      return this.ext;
+	    };
+	
+	
+	    WebPath.prototype.isRoot=function(){
+	        return this.parent === null;
+	    };
+	
+	    WebPath.prototype.path=function(){
+	        if( _.isUndefined(this._path) ){
+	            this._path = (this.isRoot() ? this.name  : this.parent.path() +  this.name) + (this.dir ? '/' : '');
+	        }
+	        return this._path;
+	    };
+	
+	    WebPath.prototype.enumerate=function(input){
+	        input = input || [];
+	        input.splice(0,0,this);
+	        if(this.parent){
+	            this.parent.enumerate(input);
+	        }
+	        return input;
+	    };
+	    WebPath.prototype.toString=function(){
+	        if(this.params){
+	            return this.path() + '?' + this.params.toString() ;
+	        }
+	        return this.path() ;
+	    };
+	
+	    WebPath.Search = Search ;
+	    WebPath.Params = Params ;
+	    WebPath.Field = Field ;
+	
+	    module.exports = WebPath;
+	})();
+	
+
 
 /***/ }
 /******/ ]);
